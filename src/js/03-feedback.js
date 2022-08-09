a405be1b613c406ccf02f0c8ff2189c24e56713c
Roman Serediuk <box4rom@gmail.com>
2022-08-09 22:07:36 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,6 +1,5 @@
 import throttle from 'lodash.throttle';
 const formEl = document.querySelector('.feedback-form');
-const formData = {};
 
 formEl.addEventListener('input', throttle(onFormIiput, 500));
 formEl.addEventListener('submit', onFormSubmit);
@@ -8,21 +7,27 @@ formEl.addEventListener('submit', onFormSubmit);
 fillFormData();
 
 function onFormIiput(evt) {
-    evt.preventDefault();
-    formData[evt.target.name] = evt.target.value;
+    const formData = {
+        email: formEl.email.value,
+        message: formEl.message.value,
+    };
     localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 }
 
 function onFormSubmit(evt) {
     evt.preventDefault();
+    const formData = {
+        email: formEl.email.value,
+        message: formEl.message.value,
+    };
     console.log(formData);
     localStorage.removeItem('feedback-form-state');
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
     evt.currentTarget.reset();
 }
 
 function fillFormData() {
     const savedData = JSON.parse(localStorage.getItem('feedback-form-state'));
-
     if (savedData) {
         formEl.email.value = savedData.email;
         formEl.message.value = savedData.message;
